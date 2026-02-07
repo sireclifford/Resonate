237d7940b0b10b776928844bb62bfc777dff0da2
Clifford Owusu <ckowusu.engineer@gmail.com>
2026-02-07 01:54:46 -0700

    Initial Commit

---
+++ Resonate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resonate
+//
+//  Created by Clifford Owusu on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resonate/ResonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonateApp.swift
+//  Resonate
+//
+//  Created by Clifford Owusu on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ResonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
